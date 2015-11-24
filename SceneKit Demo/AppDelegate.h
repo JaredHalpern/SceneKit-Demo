2aa21868e15773207c3a590d5e8471837e6f36d7
Jared Halpern <jared@byteMason.com>
2015-11-23 20:51:56 -0500

    Initial Commit

---
+++ SceneKit Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SceneKit Demo
+//
+//  Created by Jared Halpern on 11/23/15.
+//  Copyright © 2015 Jared Halpern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SceneKit Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SceneKit Demo
+//
+//  Created by Jared Halpern on 11/23/15.
+//  Copyright © 2015 Jared Halpern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
